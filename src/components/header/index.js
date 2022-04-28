876e8d6b1f4dd823f52fda73b9cd0223dc1e84ba
Douglas Oliveira <douglas.aguiar410@gmail.com>
2022-04-28 12:40:46 -0300

    Creating header component

---
+++ src/components/header/index.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+
+import UseMedia from "../hooks/useMedia";
+
+import "./styles.scss";
+
+export default function Header() {
+
+  const [open, setOpen] = useState(false);
+  const mobile = UseMedia('(max-width: 40rem)');
+
+  return (
+    <header className="container-header">
+      <a href="#/"><h2 className="title">Healthy Food</h2></a>
+
+      {mobile && (
+        <div className="mobile-button" onClick={() => setOpen(!open)}>
+          <div className={`${open && 'mobile-button-line'}`}/>
+          <div className={`${open && 'mobile-button-line'}`}/>
+          <div className={`${open && 'mobile-button-line'}`}/>
+        </div>
+      )}
+      
+      <nav className={`${mobile ? 'nav-mobile' : 'nav'} ${open && 'nav-mobile-active'}`}>
+        <ul>
+          <li><a href="#/" className="nav-button">HEALTHY RECIPES</a></li>
+          <li><a href="#/" className="nav-button">BLOG</a></li>
+          <li><a href="#/" className="nav-button">JOIN</a></li>
+          <li><button className='login-button'>REGISTER</button></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
