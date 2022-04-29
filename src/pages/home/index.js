0afe3a604406a3590b3a6e8d1e2a0a085db33fcd
Douglas Oliveira <douglas.aguiar410@gmail.com>
2022-04-29 09:31:14 -0300

    Creating featured services

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,6 +9,7 @@ import Food_1 from "../../assets/comida_1.svg";
 import Food_2 from "../../assets/comida_2.svg";
 import Food_3 from "../../assets/comida_3.svg";
 import Food_4 from "../../assets/comida_4.svg";
+import Services from "../../assets/bloco_services.svg";
 
 export default function Home() {
   return (
@@ -27,12 +28,38 @@ export default function Home() {
       </section>
       <section className="container-recipes">
         <h2>Our Best Recipes</h2>
-        <span>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts</span>
+        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts</p>
         <div className="content-cards-recipes">
           <CardRecipe image={Food_1} title={"Broccoli Salad with Bacon"} />
           <CardRecipe image={Food_2} title={"Classic Beef Burgers"} />
           <CardRecipe image={Food_3} title={"Classic Potato Salad"} />
           <CardRecipe image={Food_4} title={"Cherry Cobbler on the Grill"} />
+        </div>
+      </section>
+      <section className="container-service">
+        <img className="image-service" src={Services} alt="" />
+        <div className="content-service">
+          <h2>
+            The best services ready
+            <br/>
+            To serve you
+          </h2>
+          <br/>
+          <p>
+            Far far away, behind the word mountains, far from
+            the countries Vokalia and Consonantia, there live the
+            blind texts.
+            <br/>
+            <br/>
+            Separated they live in Bookmarksgrove right at the
+            coast of the Semantics, a large language ocean.
+            <br/>
+            <br/>
+            A small river named Duden flows by their place and
+            supplies it with the necessary regelialia.
+          </p>
+          <br/>
+          <button>Know More</button>
         </div>
       </section>
     </>
