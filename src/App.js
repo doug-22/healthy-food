ea0d31c682048196710437bccd75e0ddd966fa12
Douglas Oliveira <douglas.aguiar410@gmail.com>
2022-04-30 12:33:41 -0300

    Creating footer component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./global.scss";
 
 import Header from "./components/header";
 import Home from "./pages/home";
+import Footer from "./components/Footer";
 
 export default function App() {
   return (
     <>
       <Header />
       <Home />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./styles.scss";
+
+export default function Footer() {
+
+  return (
+    <footer className="container-footer">
+      <p>© Copyrights 2019 Stack. All Rights Reserved. </p>
+      <div className="content-terms">
+        <p>Privacy Policy</p>
+        <p>Terms and Conditions</p>
+      </div>
+    </footer>
+  );
+}
